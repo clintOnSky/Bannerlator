35fd80d7d03b1bbc3a3bf5f2cff4f699bda8b8ed
The412Banner <the412banner@users.noreply.github.com>
2026-06-27 17:31:25 -0400

    fix(shortcuts): import Box in SpecCardComponents (#19 CI fix)

--- app/src/main/java/com/winlator/star/ui/screens/SpecCardComponents.kt
+++ app/src/main/java/com/winlator/star/ui/screens/SpecCardComponents.kt
@@ -2,6 +2,7 @@ package com.winlator.star.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
